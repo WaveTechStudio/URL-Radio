b8eae589fd6b2eacb4ecdeeb210163bce83310f6
Jamal <jamal2362@googlemail.com>
2019-07-27 19:17:46 +0200

    Revert to default state

--- app/src/main/java/com/jamal2367/urlradio/URLRadio.java
+++ app/src/main/java/com/jamal2367/urlradio/URLRadio.java
@@ -33,7 +33,6 @@ public class URLRadio extends Application {
         super.onCreate();
 
         // set Day / Night theme state
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         NightModeHelper.restoreSavedState(this);
 
 // todo remove
