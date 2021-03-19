8d964ff03c1c48410d3fb29f894f804364472ddf
Jamal <jamal2362@googlemail.com>
2021-03-19 08:20:08 +0100

    reverses order in of Station and Now Playing metadata in notification

--- app/src/main/java/com/jamal2367/urlradio/helpers/FileHelper.kt
+++ app/src/main/java/com/jamal2367/urlradio/helpers/FileHelper.kt
@@ -428,7 +428,7 @@ object FileHelper {
 
         // check if file exists
         val file: File = File(context.getExternalFilesDir(folder), fileName)
-        if (!file.exists()) {
+        if (!file.exists() || !file.canRead()) {
             return String()
         }
         // readSuspended until last line reached
